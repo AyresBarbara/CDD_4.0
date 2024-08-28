cdf4d577b3a333d10ff3d0f5f9bcc8ee2056125c
AyresBarbara <ayresbarbara2@gmail.com>
2024-08-28 09:53:17 -0300

    Aula de PYTHON

---
+++ Python/exercicio01.py
@@ -0,0 +1,4 @@
+nome = input("Digite o primeiro nome: ")
+nome1 = input("Digite o segundo nome: ")
+nome2 = input("Digite o terceiro nome: ")
+print("O primeiro nome é", nome, ", o segundo nome é", nome1, "e o terceiro nome é", nome2, ".")
