6a97ceb5c7dafdb24c5c387272be38c0aafbdefa
AyresBarbara <ayresbarbara2@gmail.com>
2024-08-28 10:40:48 -0300

    Exercicio 02

---
+++ Python/exercicio02.py
@@ -0,0 +1,8 @@
+#Receba 2 notas, mostre a media e o nome
+nome = input("Digite seu nome: ")
+nota1 = int(input("Digite a nota 01: "))
+nota2 = int(input("Digite a nota 02: "))
+
+media = (nota1+nota2)/2
+
+print(" A média do aluno", nome, " é ", media)
