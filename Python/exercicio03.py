fe6cbe8eb29ef1b52e7fd3ee66a5f84a106ac0cb
AyresBarbara <ayresbarbara2@gmail.com>
2024-08-28 11:12:28 -0300

    Exercicio 03

---
+++ Python/exercicio03.py
@@ -0,0 +1,7 @@
+#Ler o nome de uma pessoa, sua idade e seu salario, mostre essas informaçoes na tela
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+salario = float(input("Digite seu salário: "))
+
+print("A ", nome," tem ", idade, " anos e recebe um salário de ", salario, "reais")
