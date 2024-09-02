5f2cc602543faefd6f339585ab2c434ecb660959
AyresBarbara <ayresbarbara2@gmail.com>
2024-09-02 09:46:24 -0300

    if e else

---
+++ Python/exercicio07.py
@@ -0,0 +1,11 @@
+#Receba 3 numéros e verifique se o aluno ta aprovado ou reprovado. Média igual a 7!
+nota1 = float(input("Informe sua nota 1: "))
+nota2 = float(input("Informe sua nota 2: "))
+nota3 = float(input("Informe sua nota 3: "))
+
+media = (nota1+ nota2+ nota3)/3
+
+if media >= 7:
+    print("Aluno Aprovado com média", media,"!")
+else:
+    print("Aluno Reprovado com média", media,"!")
