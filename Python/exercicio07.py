97f9133aa33ab55a4d764cc032c22926ff0b3366
AyresBarbara <ayresbarbara2@gmail.com>
2024-09-02 10:46:24 -0300

    Att

--- Python/exercicio07.py
+++ Python/exercicio07.py
@@ -8,4 +8,7 @@ media = (nota1 + nota2 + nota3)/3
 if media >= 7:
     print("Aluno Aprovado com média", media, "!")
 else:
-    print("Aluno Reprovado com média", media, "!")
+    if media >=4:
+        print("Aluno em recuperação com média", media, "!")
+    else:
+        print("Aluno Reprovado com média", media, "!")
