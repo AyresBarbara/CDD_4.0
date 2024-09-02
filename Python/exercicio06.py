3129062722a62e002f17ff0170b167a6d9def809
AyresBarbara <ayresbarbara2@gmail.com>
2024-09-02 09:30:57 -0300

    if e else

--- Python/exercicio06.py
+++ Python/exercicio06.py
@@ -1,7 +1,10 @@
 num1 = int(input("Digite um número: "))
 num2 = int(input("Digite outro numero: "))
 
-if num1 > num2:
-    print(num2, num1)
+if num1 == num2:
+    print("Os números digitados são iguais!")
 else:
-    print(num1, num2)
+    if num1 > num2:
+        print(num2, num1)
+    else:
+        print(num1, num2)
