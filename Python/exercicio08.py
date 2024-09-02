a79ef220318c9f5fc5b353d4e312847f4852e56b
AyresBarbara <ayresbarbara2@gmail.com>
2024-09-02 11:15:04 -0300

    elif

--- Python/exercicio08.py
+++ Python/exercicio08.py
@@ -1,12 +1,12 @@
+# Receba 2 times e o numero de gols. Imprima quem venceu ou se deu empate
 time1 = input("Digite o nome do time: ")
-gol1 = int(input("Digite quantos gols: "))
+gol1 = int(input(f"Digite quantos gols o {time1} fez: "))
 time2 = input("Digite o nome do time: ")
-gol2 = int(input("Digite quantos gols: "))
+gol2 = int(input(f"Digite quantos gols o {time2} fez: "))
 
 if gol1 == gol2:
     print("O jogo deu EMPATE!")
 elif gol1 < gol2:
-    print("O time ", time2, "é o vencedor!")
+    print(f"O time {time2} é o vencedor!")
 else:
-    print("O time ", time1, "é o vencedor!")
-    
+    print(f"O time {time1} é o vencedor!")
