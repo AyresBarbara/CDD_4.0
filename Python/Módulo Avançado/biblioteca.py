012b6b758ba31354c96ba8d6f43c12886be0b981
AyresBarbara <ayresbarbara2@gmail.com>
2024-10-02 11:38:26 -0300

    Funções

--- Python/Módulo Avançado/biblioteca.py
+++ Python/Módulo Avançado/biblioteca.py
@@ -1,16 +1,21 @@
-"""n = int(input("Digite um número: "))"""
-
+#piramide 1\22\333
 def piramide(n):
     for x in range(0, n+1):
         for y in range(x):
             print(x, end= " ")
         print()
 
+#piramide 1\12\123
 def piramide2(n):
-    for x in range(1, n+1):
+    for x in range(1,n+1):
         for y in range(1, x+1):
             print(y, end= " ")
         print()
 
-"""numero = int(input("Digite seu número: "))
-piramide(numero)"""
+#Contar quantas vogais tem no texto
+def lerVogais(frase):
+    vogais =0
+    for x in frase:
+        if x in "aeiou":
+            vogais +=1
+    print(f"Tem {vogais} vogais")
--- Python/Módulo Avançado/exercicio2.py
+++ Python/Módulo Avançado/exercicio2.py
@@ -1,6 +1,9 @@
-from biblioteca import piramide, piramide2
+from biblioteca import *
 
 n = int(input("Digite um número: "))
 
 piramide(n)
 piramide2(n)
+
+frase =  ("O rato roeu a roupa do rei de Roma").lower()
+lerVogais(frase)
