d174b20dcc62323e914a964328bc9fe0f6d0d03a
AyresBarbara <ayresbarbara2@gmail.com>
2024-10-04 17:19:57 -0300

    Atividade para entregar

--- Python/Módulo Avançado/exercicio9.py
+++ Python/Módulo Avançado/exercicio9.py
@@ -6,26 +6,44 @@ tam = len(usuario)
 menu = 0
 while menu != 4:
     menu = int(input("Digite a opção desejada: \n 1 - Cadastro \n 2 - Login \n 3 - Mostrar usuários \n 4 - Sair\nDigite sua opção:"))
+    print()
 
     match menu:
 
         case 1:
             for x in range(tam):
-                usuario[x] = input(f"Cadastre o nome do usuário [{x+1}]: ")
-                senha[x] = input(f"Cadastre a senha do usuario [{x+1}]: ")
-            print("Cadastro realizado com sucesso!")
+                while True:
+                    user = input(f"Cadastre o nome do usuário [{x+1}]: ")
+                    if user.strip() == "":
+                        print("Usuário inválido! Adicione um nome válido!")
+                        continue
+                    if user in usuario:
+                        print("Usuário já cadastrado. Tente outro nome!\n")
+                        continue
 
+                    password = input(f"Cadastre a senha do usuario [{x+1}]: ")
+                    if password in senha:
+                        print("Senha já cadastrado. Tente outra senha!\n")
+                        continue
+                    if password.strip() == "":
+                        print("Senha inválido! Adicione uma senha válida!")
+                        continue
+
+                    usuario[x] = user
+                    senha[x] = password
+                    break
+            print("Cadastro realizado com sucesso!\n")
         case 2:
 
             tent = 3
             while tent > 0:
-                usuarioTent = input(f"Digite o nome do usuário: ")
-                senhaTent = input(f"Digite a senha do usuário: ")
+                usuarioTent = input(f"Digite o nome do usuário: ").strip()
+                senhaTent = input(f"Digite a senha do usuário: ").strip()
                 login = False
 
                 for x in range(tam):
                     if usuario[x] == usuarioTent and senha[x] == senhaTent:
-                        print("Login efetuado com sucesso!")
+                        print("Login efetuado com sucesso!\n")
                         login = True
                         break
                 if login:
@@ -38,13 +56,14 @@ while menu != 4:
                 print("Conta bloqueada! \nVocê excedeu o número de tentativas, procure a sua agência. ")
 
         case 3:
-
+            print("Usuários Cadastrados: ")
             for x in range(tam):
-                print(f"Nome do usuário: {usuario[x]} \nSenha: {senha[x]} \nPosição: {x+1}\n")
+                if usuario[x] and senha[x]:
+                    print(f"Nome do usuário: {usuario[x]} \nSenha: {senha[x]} \nPosição: {x+1}\n")
 
         case 4:
             print("Operação Encerrada!")
             break
 
         case _:
-            print("Opção inválida! Tente novamente.")
+            print("Opção inválida! Tente novamente.\n")
