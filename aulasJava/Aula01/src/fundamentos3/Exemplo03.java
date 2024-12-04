4d7cb1890782f29acfe70a45706e43f584bcda58
AyresBarbara <ayresbarbara2@gmail.com>
2024-12-04 08:56:16 -0300

    toLowerCase_toUpperCase

--- aulasJava/Aula01/src/fundamentos3/Exemplo03.java
+++ aulasJava/Aula01/src/fundamentos3/Exemplo03.java
@@ -4,12 +4,18 @@ public class Exemplo03 {
 
 	public static void main(String[] args) {
 		String str = "Hello World";
+		//imprimie a partir da 6º posição
 		String resultado = str.substring(6);
 		System.out.println(resultado);
-		
+		//imprime a partir da posição indicada, até o final da palavra
 		String resultado1 = str.substring(3,8);
 		System.out.println(resultado1);
-
+		//Maiúsculo
+		String resultado2 = str.toUpperCase();
+		System.out.println(resultado2);
+		//Minusculo
+		String resultado3 = str.toLowerCase();
+		System.out.println(resultado3);
 	}
 
 }
