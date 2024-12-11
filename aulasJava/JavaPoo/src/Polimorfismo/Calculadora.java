926a1cb5ffcbe21777ae651621a94512b7168f16
AyresBarbara <ayresbarbara2@gmail.com>
2024-12-11 11:20:55 -0300

    Polimorfismo

---
+++ aulasJava/JavaPoo/src/Polimorfismo/Calculadora.java
@@ -0,0 +1,12 @@
+package Polimorfismo;
+
+public class Calculadora {
+	
+	public void somar(double a,double  b) {
+		System.out.println(a+b);
+	}
+	public void somar(double a,double  b, double c) {
+		System.out.println(a+b+c);
+	}
+}
+
---
+++ aulasJava/JavaPoo/src/Polimorfismo/Teste.java
@@ -0,0 +1,10 @@
+package Polimorfismo;
+
+public class Teste {
+	public static void main(String[] args) {
+		Calculadora c1 = new Calculadora();
+		c1.somar(1,2);
+		c1.somar(5, 7, 50);
+	}
+
+}
